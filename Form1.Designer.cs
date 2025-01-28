c973cec583eade6b987ae7bceaf5bec4df46236f
Caner Mollaoğlu <canermollaoglu@hotmail.com>
2025-01-28 15:31:48 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WorkshopWindowsProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnEkle = new Button();
+            lstListe = new ListBox();
+            SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            btnEkle.Location = new Point(23, 327);
+            btnEkle.Name = "btnEkle";
+            btnEkle.Size = new Size(233, 65);
+            btnEkle.TabIndex = 0;
+            btnEkle.Text = "EKLE";
+            btnEkle.UseVisualStyleBackColor = true;
+            // 
+            // lstListe
+            // 
+            lstListe.FormattingEnabled = true;
+            lstListe.ItemHeight = 37;
+            lstListe.Location = new Point(23, 21);
+            lstListe.Name = "lstListe";
+            lstListe.Size = new Size(233, 300);
+            lstListe.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(15F, 37F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(308, 433);
+            Controls.Add(lstListe);
+            Controls.Add(btnEkle);
+            Font = new Font("Segoe UI", 20F);
+            Margin = new Padding(6, 7, 6, 7);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnEkle;
+        private ListBox lstListe;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace WorkshopWindowsProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
